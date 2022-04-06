8b554b4087e1d76f013952baff8e0dbcbacd9ccd
Makinde Foluso Samuel <folusosamuel@gmail.com>
2022-04-06 15:10:18 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hello from the server side", app: "Natours" });
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port  $(port)....`);
+});
