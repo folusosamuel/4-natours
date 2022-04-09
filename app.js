c25a0b0624afe615d5c283c16f3cd3ec2cee419c
Makinde Foluso Samuel <folusosamuel@gmail.com>
2022-04-09 19:42:20 +0100

    L52: Starting Our API updated!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import express from "express";
 const app = express();
+const __dirname = "./";
 
 // app.get("/", (req, res) => {
 //   res
@@ -12,9 +13,17 @@ const app = express();
 //   res.send("You can post to this endpoint....");
 // });
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-// );
+const tours = JSON.parse(
+  fs.readFileSync(
+    `${__dirname}/starter/dev-data/data/tours-simple.json`,
+    //`./starter/dev-data/data/tours-simple.json`,
+    function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    }
+  )
+);
 
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
