10f9a99791b7e055c5ec57ee497e048a13304313
Makinde Foluso Samuel <folusosamuel@gmail.com>
2022-04-20 10:53:59 +0100

    LC62: Creating and Mounting Multiple Routers

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import * as fs from "fs";
 import express from "express";
-
 import morgan from "morgan";
 const app = express();
 const __dirname = "./";
@@ -36,128 +35,128 @@ const tours = JSON.parse(
 
 //2. ROUTE HANDLERS
 
-// const getAllTours = (req, res) => {
-//   res.status(200).json({
-//     status: "success",
-//     results: tours.length,
-//     data: {
-//       tours,
-//     },
-//   });
-// };
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
-// const createTour = (req, res) => {
-//   const newId = tours[tours.length - 1].id + 1;
-//   const newTour = Object.assign({ id: newId }, req.body); //create new object with the new id and body
-//   tours.push(newTour); //push the new object into the existing array of objects
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body); //create new object with the new id and body
+  tours.push(newTour); //push the new object into the existing array of objects
 
-//   fs.writeFile(
-//     `${__dirname}/dev-data/data/tours-simple.json`,
-//     JSON.stringify(tours),
-//     (err) => {
-//       res.status(201).json({
-//         status: "success",
-//         data: {
-//           tour: newTour,
-//         },
-//       });
-//     }
-//   );
-// };
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
 
-// const getTour = (req, res) => {
-//   const id = req.params.id * 1; //convert the params numbers which are strings into number by multiplying by 1
-//   const tour = tours.find((el) => el.id === id);
+const getTour = (req, res) => {
+  const id = req.params.id * 1; //convert the params numbers which are strings into number by multiplying by 1
+  const tour = tours.find((el) => el.id === id);
 
-//   if (!tour) {
-//     return res.status(404).json({
-//       status: "fail",
-//       message: "invalid ID",
-//     });
-//   }
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "invalid ID",
+    });
+  }
 
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       tours: tour, //or simply use "tour" only on this line
-//     },
-//   });
-// };
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours: tour, //or simply use "tour" only on this line
+    },
+  });
+};
 
-// const updateTour = (req, res) => {
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: "fail",
-//       message: "invalid ID",
-//     });
-//   }
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       tour: "<Updated tour here...>",
-//     },
-//   });
-// };
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "invalid ID",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
+};
 
-// const deleteTour = (req, res) => {
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: "fail",
-//       message: "invalid ID",
-//     });
-//   }
-//   res.status(204).json({
-//     status: "success",
-//     data: {
-//       tour: null,
-//     },
-//   });
-// };
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "invalid ID",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: {
+      tour: null,
+    },
+  });
+};
 
-// const getAllUsers = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined!",
-//   });
-// };
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
 
-// const getUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined!",
-//   });
-// };
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
 
-// const createUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined!",
-//   });
-// };
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
 
-// const updateUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined!",
-//   });
-// };
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
 
-// const deleteUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined!",
-//   });
-// };
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
 
 //3. ROUTE
 //We create a router for each resource. Ex: When the request hits tours,
 // it will be managed by tourRouter.
 //Tours
-// app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/tours", tourRouter);
 
-// tourRouter.route("/").get(getAllTours).post(createTour);
+tourRouter.route("/").get(getAllTours).post(createTour);
 
-// tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 //Users
 app.use("/api/v1/users", userRouter);
